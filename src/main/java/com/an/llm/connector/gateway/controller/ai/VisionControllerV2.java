70432f7679b5afa7eddfec4f9b6c5ea762c3ec91
Ashar581 <asharshahab@gmail.com>
2026-06-01 01:47:51 +0530

    Release: Released v2 for Vision for testing. Agents now using v2 instead of v1.

--- src/main/java/com/an/llm/connector/gateway/controller/ai/VisionControllerV2.java
+++ src/main/java/com/an/llm/connector/gateway/controller/ai/VisionControllerV2.java
@@ -5,26 +5,29 @@ import com.an.llm.connector.gateway.base.BaseApiDelegate;
 import com.an.llm.connector.gateway.exception.ApiFallbackException;
 import com.an.llm.connector.gateway.model.LlmConnectorRequest;
 import com.an.llm.connector.gateway.model.classification.ClassificationResponse;
-import com.an.llm.connector.gateway.service.ai.VisionService;
-import com.an.llm.connector.gateway.service.classification.ClassificationOrchestrator;
 import com.an.llm.connector.gateway.service.ai.VisionServiceV2;
+import com.an.llm.connector.gateway.service.classification.ClassificationOrchestrator;
 import jakarta.validation.Valid;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/llm/v1/vl")
-public class VisionController extends BaseApiDelegate {
-    private final VisionService visionService;
+@RequestMapping("api/llm/v2/vl")
+public class VisionControllerV2 extends BaseApiDelegate {
+    private final VisionServiceV2 visionServiceV2;
     private final ClassificationOrchestrator classificationOrchestrator;
 
     @PostMapping("")
     public ResponseEntity<@NonNull ApiResponseBody<String>> vison(@Valid @ModelAttribute LlmConnectorRequest request){
-        return sendSuccessfulApiResponse(visionService.visionPrompt(request));
+        return sendSuccessfulApiResponse(visionServiceV2.visionPrompt(request));
     }
+    //this is still having v1 since no changes are needed yet.
     @PostMapping("classify")
     public ResponseEntity<@NonNull ApiResponseBody< @NonNull ClassificationResponse>> classify(@ModelAttribute @Valid LlmConnectorRequest request) {
         try {
@@ -33,5 +36,4 @@ public class VisionController extends BaseApiDelegate {
             throw new ApiFallbackException(e.getMessage());
         }
     }
-
 }
