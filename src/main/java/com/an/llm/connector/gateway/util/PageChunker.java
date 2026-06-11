59a36ad1f67ce011732684f206add8450671dbaf
Ashar581 <asharshahab@gmail.com>
2026-06-11 16:20:06 +0530

    Change: Changed the page chunker limit to 2.

--- src/main/java/com/an/llm/connector/gateway/util/PageChunker.java
+++ src/main/java/com/an/llm/connector/gateway/util/PageChunker.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PageChunker {
-    public static final int PAGES_PER_CHUNK = 4;
+    public static final int PAGES_PER_CHUNK = 2;
 
     public static List<List<byte[]>> chunk(List<byte[]> pages) {
         List<List<byte[]>> chunks = new ArrayList<>();
