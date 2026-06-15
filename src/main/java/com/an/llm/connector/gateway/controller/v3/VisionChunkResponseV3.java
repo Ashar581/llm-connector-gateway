3322d9d5f8c9fa08f0fa573915ab7d481a186123
Ashar581 <asharshahab@gmail.com>
2026-06-15 10:31:34 +0530

    Remove & Change: Removed the v3 package for vision. Changed Q5 to Q4.

--- src/main/java/com/an/llm/connector/gateway/controller/v3/VisionChunkResponseV3.java
+++
@@ -1,8 +0,0 @@
-package com.an.llm.connector.gateway.controller.v3;
-
-public record VisionChunkResponseV3(
-        int chunkNumber,
-        int primaryPage,
-        Integer previousContextPage,
-        String response
-) {}
