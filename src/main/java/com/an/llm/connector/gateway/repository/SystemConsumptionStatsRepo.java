a38f083c7335a953ac4485072716275b93f1c5b4
Ashar581 <asharshahab@gmail.com>
2026-06-17 10:53:24 +0530

    Optimized: Optimised the filteration for stats. Due to .findAll(specifications) the indexing wasnt working.

--- src/main/java/com/an/llm/connector/gateway/repository/SystemConsumptionStatsRepo.java
+++ src/main/java/com/an/llm/connector/gateway/repository/SystemConsumptionStatsRepo.java
@@ -1,15 +1,32 @@
 package com.an.llm.connector.gateway.repository;
 
-import com.an.llm.connector.gateway.dto.SystemConsumptionStatsDto;
 import com.an.llm.connector.gateway.entity.SystemConsumptionStatsEntity;
-import com.an.llm.connector.gateway.enums.LlmModels;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.Instant;
 import java.util.List;
 
-public interface SystemConsumptionStatsRepo extends JpaRepository<@NonNull SystemConsumptionStatsEntity,@NonNull Long> , JpaSpecificationExecutor<@NonNull SystemConsumptionStatsEntity> {
+public interface SystemConsumptionStatsRepo extends JpaRepository<@NonNull SystemConsumptionStatsEntity,@NonNull Long> {
     List<SystemConsumptionStatsEntity> findByCreatedAtBetween(Instant start, Instant end);
+
+    @Query(value = """
+    SELECT *
+    FROM system_consumption_stats_entity
+    WHERE
+    (NULLIF(:agentName, '') IS NULL OR agent_name = :agentName)
+    AND (NULLIF(:modelName, '') IS NULL OR model_name = :modelName)
+    AND (NULLIF(:server, '') IS NULL OR server = :server)
+    AND (CAST(:startDate AS timestamptz) IS NULL OR created_at >= :startDate)
+    AND (CAST(:endDate AS timestamptz) IS NULL OR created_at <= :endDate)
+    """, nativeQuery = true)
+    List<SystemConsumptionStatsEntity> filter(
+            @Param("agentName") String agentName,
+            @Param("modelName") String modelName,
+            @Param("server") String server,
+            @Param("startDate") Instant startDate,
+            @Param("endDate") Instant endDate
+    );
 }
