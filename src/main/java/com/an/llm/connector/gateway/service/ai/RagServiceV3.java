2094a0bc2bffa4160a9eeb403900d82982e48c8f
Ashar581 <asharshahab@gmail.com>
2026-07-24 14:35:16 +0530

    Add: Added general internet answers inclusive in RAG. Relying pon AI to decide inclusing/exclusion. (STREAM)

--- src/main/java/com/an/llm/connector/gateway/service/ai/RagServiceV3.java
+++ src/main/java/com/an/llm/connector/gateway/service/ai/RagServiceV3.java
@@ -216,6 +216,10 @@ public class RagServiceV3 {
             }
         }
 
+        String instructions = (request.getInstructions() == null || request.getInstructions().isBlank())
+                ? LlmInstructions.DEFAULT_RAG_INSTRUCTION
+                : request.getInstructions();
+
         ConversationIntelligence intelligence = conversationIntelligenceService.analyse(request);
         log.info("Conversation Intelligence (stream): {}", intelligence);
         List<Document> retrievedChunks;
@@ -239,24 +243,16 @@ public class RagServiceV3 {
         //testing
         boolean shouldSearchInternet = Boolean.TRUE.equals(intelligence.getInternetMayBeHelpful()) && Boolean.FALSE.equals(request.getEnablePrivateMode());
 
-        if (shouldSearchInternet && (retrievedChunks.isEmpty() || !confidenceService.hasUsableContext(retrievedChunks))) {
+        if (shouldSearchInternet && !confidenceService.hasUsableContext(retrievedChunks)) {
             String webResultsSummarized = webSearchService.search(
                     new SearchRequest(intelligence.getRewrittenQuery(),3, Duration.of(10000, ChronoUnit.SECONDS)),
                     request
             );
             if (!webResultsSummarized.isBlank()) {
+                instructions+="\n-DATA FROM INTERNET - Should only be used as context if you think it needed. Prefer the RAG context but if needed then add the web data too.";
                 context += "DATA FROM INTERNET: \n" + webResultsSummarized;
             }
         }
-
-
-        if (!confidenceService.hasUsableContext(retrievedChunks) && !request.isChatHistoryEnabled()) {
-            return Flux.just("I am afraid I don't know how to answer that.");
-        }
-
-        String instructions = (request.getInstructions() == null || request.getInstructions().isBlank())
-                ? LlmInstructions.DEFAULT_RAG_INSTRUCTION
-                : request.getInstructions();
 
         long start = System.currentTimeMillis();
 
