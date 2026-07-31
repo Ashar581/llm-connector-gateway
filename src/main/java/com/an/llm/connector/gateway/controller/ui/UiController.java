2a0ba664d9a69ed4acada669733578dd403cb8a9
Ashar581 <asharshahab@gmail.com>
2026-07-31 14:04:28 +0530

    Release: Released the llm ui and supporting controller for it. Now running springboot will also run the UI.

--- src/main/java/com/an/llm/connector/gateway/controller/ui/UiController.java
+++ src/main/java/com/an/llm/connector/gateway/controller/ui/UiController.java
@@ -1,4 +1,22 @@
 package com.an.llm.connector.gateway.controller.ui;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UiController {
+
+    @GetMapping({
+            "/ui/",
+            "/ui",
+            "/ui/{path:[^\\.]*}"
+    })
+    public String forward() {
+        return "forward:/ui/index.html";
+    }
+
+    @GetMapping("/")
+    public String redirectToUi() {
+        return "redirect:/ui";
+    }
 }
