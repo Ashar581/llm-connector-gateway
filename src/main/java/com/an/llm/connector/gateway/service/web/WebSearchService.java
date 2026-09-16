55ce51e56c984aaf55abd7a2a0bcc5e91eced514
Ashar581 <asharshahab@gmail.com>
2026-09-16 14:17:55 +0530

    Remove: Removed a log from web search service.

--- src/main/java/com/an/llm/connector/gateway/service/web/WebSearchService.java
+++ src/main/java/com/an/llm/connector/gateway/service/web/WebSearchService.java
@@ -204,8 +204,6 @@ public class WebSearchService {
 
         SearchResponse response = searXNGSearch.search(internetQuery,website);
 
-        log.info("Search Response: {}",response);
-
         List<String> urls = response.results()
                 .stream()
                 .limit(searchRequest.maxResults()==null? 5 : searchRequest.maxResults())
